f0f4e6b7472be2ded7d115a2acc20ed3a608d2f1
javera91 <146044960+javera91@users.noreply.github.com>
2024-04-24 19:29:09 -0500

    c1
    c1

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,14 +1,27 @@
 import { Controller, Get, Post, HttpCode, Param, Body } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
 
 @Controller('cats')
 export class CatsController {
+    constructor(private catsService: CatsService){}
+
+    @Post()
+    async create(@Body() createCatDto: CreateCatDto){
+        }
+    @Get()
+    async findAll(): Promise<Cat[]>{
+        return this.catsService.findAll();
+    }
+}
+
     /*@Post()
     @HttpCode(418)
     create() {
         return 'Soy una tetera';
         }*/
-    @Get()
+    /*@Get()
     findAll(): string{
         return 'This action returns all cats';
     }
@@ -20,5 +33,4 @@ export class CatsController {
     async create(@Body() createCatDto: CreateCatDto) {
         return `This action adds a new cat called ${createCatDto.name}.`;
         //return `This is the age ${createCatDto.age}.`;
-    }
-}
+    }*/
